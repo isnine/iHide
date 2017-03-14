aec80e0517117ded8ef8baa1ce97acaf179a085c
Nine <245328522@qq.com>
2017-03-14 17:25:00 +0800

    init

--- iHide/main.m
+++ iHide/main.m
@@ -9,5 +9,20 @@
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, const char * argv[]) {
+    system("defaults read com.apple.finder AppleShowAllFiles > /tmp/visible.tmp");
+    FILE *f = fopen("/tmp/visible.tmp", "r");
+    if (f != NULL) {
+        int i;
+        fscanf(f, "%d", &i);
+        if (i == 0) {
+            system("defaults write com.apple.finder AppleShowAllFiles -bool true");
+        } else {
+            system("defaults write com.apple.finder AppleShowAllFiles -bool false");
+        }
+        system("killall Finder");
+        fclose(f);
+        system("rm /tmp/visible.tmp");
+    }
+    exit(0);
     return NSApplicationMain(argc, argv);
 }
